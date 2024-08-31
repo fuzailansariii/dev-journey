450afe025928cbab0555af29aa028aedf251b2fa
fuzailansariii <fuzailansarisecret@gmail.com>
2024-08-31 20:33:11 +0300

    minor changes

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,16 +1,8 @@
 "use client";
-import { verifySchema } from "@/schemas/verificationCodeSchema";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function page() {
-  const form = useForm<z.infer<typeof verifySchema>>({
-    resolver: zodResolver(verifySchema),
-  });
-
   return (
     <form className="flex justify-center items-center w-screen fixed h-screen bg-slate-200 overflow-hidden">
       <div className="w-full sm:w-2/6 bg-white py-16 rounded-xl border shadow-md">
